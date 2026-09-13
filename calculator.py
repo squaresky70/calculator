9e0855bf6f7c0e5a5881ed26ac2c931e4335eea0
squaresky70 <squaresky70@gmail.com>
2026-09-13 21:29:57 +0900

    feat. 계산기 기본 창 생성

---
+++ calculator.py
@@ -0,0 +1,9 @@
+import tkinter as tk  # tkinter 모듈을 가져옴
+
+# 메인 창 만들기
+window = tk.Tk()
+window.title("계산기")  # 창 제목 설정
+window.geometry("300x400")  # 창 크기 설정 (가로x세로)
+
+# 창을 계속 띄워두기 위한 반복문
+window.mainloop()
