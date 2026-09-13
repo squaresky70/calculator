3fcca86a0d7d7b50e51cb155bad696ae6eaabe8d
squaresky70 <squaresky70@gmail.com>
2026-09-13 21:33:04 +0900

    feat: 결과 표시창 추가

--- calculator.py
+++ calculator.py
@@ -5,5 +5,9 @@ window = tk.Tk()
 window.title("계산기")  # 창 제목 설정
 window.geometry("300x400")  # 창 크기 설정 (가로x세로)
 
+# 계산 결과를 보여줄 입력창(Entry) 만들기
+display = tk.Entry(window, justify="right", font=("Arial", 24))
+display.pack(fill="x", padx=10, pady=10)  # 가로로 꽉 채우고 여백 주기
+
 # 창을 계속 띄워두기 위한 반복문
 window.mainloop()
